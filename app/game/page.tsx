a68f4c28bf731767cbc548ecff68ff58fd044094
elfamosoworker <varmaanthony@gmail.com>
2025-05-16 15:42:25 +0200

    add waiting for new page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,8 +3,12 @@ import { Header } from '@/components/Header';
 import DarkModeInitializer from '@/components/DarkModeInitializer';
 
 export default function Game() {
-    // <DarkModeInitializer />
-    // <div className="top-0 z-50 bg-[var(--background)] pl-6 pt-6 mt-1 flex flex-col md:flex-row md:justify-between md:items-center gap-4">
-    // <Header title="Projet Boulangerie" />
-    // </div>
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-300">
+      <div className="text-center p-8 bg-white rounded-2xl shadow-xl">
+        <h1 className="text-3xl text-black font-bold mb-4">Site en cours de construction</h1>
+        <p className="text-gray-600">Reviens bientôt pour découvrir nos nouveautés !</p>
+      </div>
+    </main>
+  );
 }
