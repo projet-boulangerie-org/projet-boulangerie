b930d52e23d1360067d0cd4119109886b2b83e67
elfamosoworker <varmaanthony@gmail.com>
2025-05-16 11:40:35 +0200

    add game/

---
+++ app/game/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '@/components/Header';
+import DarkModeInitializer from '@/components/DarkModeInitializer';
+
+export default function Game() {
+    // <DarkModeInitializer />
+    // <div className="top-0 z-50 bg-[var(--background)] pl-6 pt-6 mt-1 flex flex-col md:flex-row md:justify-between md:items-center gap-4">
+    // <Header title="Projet Boulangerie" />
+    // </div>
+}
