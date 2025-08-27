2c4d81fd9d4253c1dc8426b2a4c9c933bf0512e5
elfamosoworker <varmaanthony@gmail.com>
2025-08-27 21:11:17 +0200

    delete _blank link

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
         <div className="p-6 flex flex-col md:flex-row items-center gap-4">
           <ul className="flex flex-row gap-4">
             <li><a href="#equipe" className="nav-button text-base text-white">Équipe</a></li>
-            <li><a href={`${basePath}/game`} target="_blank" className="nav-button text-base text-white">Jouer</a></li>
+            <li><a href={`${basePath}/game`} className="nav-button text-base text-white">Jouer</a></li>
             <li>
-              <Link href='/croissant' target="_blank" rel="noopener noreferrer">
+              <Link href='/croissant' rel="noopener noreferrer">
                 <button className="nav-button text-base text-white">Croissanter</button>
               </Link>
             </li>
