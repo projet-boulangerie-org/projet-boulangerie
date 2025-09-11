eafff0ab019768e60012f52e3f8780fd8c9ca9a6
elfamosoworker <varmaanthony@gmail.com>
2025-09-11 23:49:39 +0200

    align footer and minors change on /croissant

--- app/croissant/page.tsx
+++ app/croissant/page.tsx
@@ -12,29 +12,24 @@ export default function CroissantPage() {
       <main className="relative mt-10 w-full flex-1 bg-black flex flex-col lg:flex-row items-start lg:items-center justify-start px-0">
         <CopyMessage />
         <div className="flex-1 w-full text-left px-8 lg:px-16">
-          {/* Ligne de viennoiseries */}
-          <div className="text-[6rem] lg:text-[8rem] text-boulange-gold space-x-2">
+          <div className="text-[6rem] text-boulange-gold space-x-2">
             🥐 🥖 🥯 🥞
           </div>
 
-          {/* Titre */}
           <h1 className="mt-8 text-6xl md:text-6xl lg:text-5xl font-medieval text-boulange-gold">
             Vous avez été croissanté !
           </h1>
 
-          {/* Texte principal */}
           <p className="mt-10 mb-10 text-lg leading-relaxed text-white">
             La Guilde de la Boulangerie a décidé de vous{' '}
             <span className="font-semibold text-boulange-gold">croissanter</span>, car vous avez oublié de
             verrouiller votre PC.
           </p>
 
-          {/* Progress */}
           <p className="mt-10 text-xl font-semibold text-boulange-gold">
             200 % complété
           </p>
 
-          {/* Bas de page espacé */}
           <div className="mt-10 space-y-6 text-sm pb-8 overflow-hidden">
             <p className="text-white">
               Pour (re)découvrir le projet,&nbsp;
@@ -47,8 +42,7 @@ export default function CroissantPage() {
           </div>
         </div>
 
-        {/* Croissant décoratif à droite, centré verticalement */}
-        <div className="flex-none ml-auto lg:mr-16 mt-12 lg:mt-0 text-[8rem] lg:text-[10rem]">
+        <div className="flex-none ml-auto lg:mr-40 mt-12 lg:mt-24 text-[8rem] lg:text-[7rem]">
           🥐
         </div>
       </main>
